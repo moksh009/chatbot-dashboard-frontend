fc50a3232561d75bf7faaf8c943dc2d8a007c841
Moksh090 <moksh@ttfaconsultancy.in>
2025-12-21 14:52:40 +0530

    new change12

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL 
-    ||'http://localhost:3000/api', 
+    ||'https://chatbot-backend-lg5y.onrender.com/api', 
 });
 
 api.interceptors.request.use((config) => {
